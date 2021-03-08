6d1d86988ef30877b232d85789e3fd6065d0fb54
bearddan2000 <67988550+bearddan2000@users.noreply.github.com>
2021-03-07 22:10:21 -0500

    Add files via upload

---
+++ bin/app/src/main/groovy/example/model/Input.groovy
@@ -0,0 +1,22 @@
+package example.model;
+
+public class Input {
+
+  private boolean isInit;
+  private int count;
+
+  public Input() {}
+
+  public Input(int count, boolean isInit) {
+    this.count = count;
+    this.isInit = isInit;
+  }
+
+  public int getCount() {
+    return this.count;
+  }
+
+  public boolean getIsInit() {
+    return this.isInit;
+  }
+}
